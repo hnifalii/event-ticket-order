7fe35f1dc6a60447d8bec10e0dde3d1b1d5030eb
MDAlviano <mdalviano.21122@gmail.com>
2025-12-15 16:46:32 +0700

    add: add footer

--- src/app/(private)/admin/add-organizer/page.jsx
+++ src/app/(private)/admin/add-organizer/page.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { events } from "@/data/events";
 import { users } from "@/data/users";
@@ -8,193 +8,260 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 const EyeIcon = (props) => (
-  <svg {...props} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+  <svg
+    {...props}
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1"
+      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+    ></path>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1"
+      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+    ></path>
   </svg>
 );
 
 const EyeOffIcon = (props) => (
-  <svg {...props} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M13.875 18.828c-1.554 1.156-3.35 1.776-5.275 1.776H4.5A2.25 2.25 0 012.25 18V6A2.25 2.25 0 014.5 3.75h4.125c1.925 0 3.721.62 5.275 1.776m-5.275 1.156A3.75 3.75 0 1018.75 12.375M12 18V6m-1.5 6L12 9l1.5 3"></path>
-    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M21 21L3 3"></path>
+  <svg
+    {...props}
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1"
+      d="M13.875 18.828c-1.554 1.156-3.35 1.776-5.275 1.776H4.5A2.25 2.25 0 012.25 18V6A2.25 2.25 0 014.5 3.75h4.125c1.925 0 3.721.62 5.275 1.776m-5.275 1.156A3.75 3.75 0 1018.75 12.375M12 18V6m-1.5 6L12 9l1.5 3"
+    ></path>
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1"
+      d="M21 21L3 3"
+    ></path>
   </svg>
 );
 
-const PasswordToggleInput = ({ label = "Password", name = "password", value, onChange }) => {
-    const [isVisible, setIsVisible] = useState(false);
-    const toggleVisibility = () => setIsVisible(prev => !prev);
-    const inputType = isVisible ? 'text' : 'password';
+const PasswordToggleInput = ({
+  label = "Password",
+  name = "password",
+  value,
+  onChange,
+}) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => setIsVisible((prev) => !prev);
+  const inputType = isVisible ? "text" : "password";
 
-    return (
-        <div>
-            <label htmlFor={name}>
-                {label}
-            </label>
-            <div className="relative mt-1">
-                <input
-                    id={name}
-                    name={name}
-                    type={inputType}
-                    value={value}
-                    onChange={onChange}
-                    className="focus:outline-none focus:ring-2 ring-1 focus:ring-[#7209b7]/80 rounded-md p-2 appearance-none block w-full px-3 py-2 pr-10"
-                />
-                
-                <button
-                    type="button"
-                    onClick={toggleVisibility}
-                    className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 text-black focus:outline-none"
-                    aria-label={isVisible ? "Sembunyikan password" : "Tampilkan password"}
-                >
-                    {isVisible ? (
-                        <EyeOffIcon className="h-5 w-5" />
-                    ) : (
-                        <EyeIcon className="h-5 w-5" />
-                    )}
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <div className="relative mt-1">
+        <input
+          id={name}
+          name={name}
+          type={inputType}
+          value={value}
+          onChange={onChange}
+          className="focus:outline-none focus:ring-2 ring-1 focus:ring-[#7209b7]/80 rounded-md p-2 appearance-none block w-full px-3 py-2 pr-10"
+        />
+
+        <button
+          type="button"
+          onClick={toggleVisibility}
+          className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5 text-black focus:outline-none"
+          aria-label={isVisible ? "Sembunyikan password" : "Tampilkan password"}
+        >
+          {isVisible ? (
+            <EyeOffIcon className="h-5 w-5" />
+          ) : (
+            <EyeIcon className="h-5 w-5" />
+          )}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 const ErrorMessage = ({ message }) => (
-    <p className="text-red-500 text-sm p-3 bg-red-100 rounded-sm border-s-[3px]">{message}</p>
+  <p className="text-red-500 text-sm p-3 bg-red-100 rounded-sm border-s-[3px]">
+    {message}
+  </p>
 );
 
 export default function Page() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
-    const [eventId, setEventId] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [eventId, setEventId] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-    const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const search = useSearchParams();
-    const id = search.get("eventId");
+  const search = useSearchParams();
+  const id = search.get("eventId");
 
-    const selectedEvent = events.find((e) => e.id == id);
+  const selectedEvent = events.find((e) => e.id == id);
 
-    const API_URL = "https://capsular-peddlingly-immanuel.ngrok-free.dev";
+  const API_URL = "https://capsular-peddlingly-immanuel.ngrok-free.dev";
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setLoading(true);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
 
-        try {
-            if (username == "" || password == "" || name == "" || eventId == "") {
-                setError("Mohon isi semua field yang tersedia!");
-                return;
-            }
+    try {
+      if (username == "" || password == "" || name == "" || eventId == "") {
+        setError("Mohon isi semua field yang tersedia!");
+        return;
+      }
 
-            if (password.length < 8) {
-                setError("Panjang minimal password adalah 8");
-                return;
-            }
+      if (password.length < 8) {
+        setError("Panjang minimal password adalah 8");
+        return;
+      }
 
-            const response = await fetch(API_URL, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    username: username,
-                    password: password,
-                    name: name,
-                    eventId: eventId, 
-                }),
-            });
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password,
+          name: name,
+          eventId: eventId,
+        }),
+      });
 
-            if (!response.ok) {
-                setError(`Server error: HTTP ${response.status}`);
-                return;
-            }
+      if (!response.ok) {
+        setError(`Server error: HTTP ${response.status}`);
+        return;
+      }
 
-            const result = await response.json();
+      const result = await response.json();
 
-            if (result && result.success === true) {
-                router.push('/admin');
-                router.refresh();
-                toast.success("Berhasil!");
-            } else {
-                setError(result.data || result.message || "Failed to create new event.");
-                return;
-            }
-        } catch (err) {
-            setError("Kesalahan saat mengirim/memproses:", err);
-        } finally {
-            setLoading(false);
-        }
+      if (result && result.success === true) {
+        router.push("/admin");
+        router.refresh();
+        toast.success("Berhasil!");
+      } else {
+        setError(
+          result.data || result.message || "Failed to create new event."
+        );
+        return;
+      }
+    } catch (err) {
+      setError("Kesalahan saat mengirim/memproses:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const testSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const newOrganizer = {
+      id: 5,
+      username: username,
+      password: password,
+      name: name,
+      eventId: eventId,
     };
 
-    const testSubmit = (e) => {
-        e.preventDefault();
-        setLoading(true);
+    users.push(newOrganizer);
 
-        const newOrganizer = {
-            id: 5, 
-            username: username,
-            password: password,
-            name: name,
-            eventId: eventId,
-        };
+    router.push("/admin");
+    router.refresh();
+  };
 
-        users.push(newOrganizer);
-
-        router.push('/admin');
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/auth");
+      router.refresh();
+    } else {
+      if (session !== undefined && session?.user.role !== "admin") {
+        router.push("/auth");
         router.refresh();
+      }
     }
+  }, [router, session, status, session?.user.role]);
 
-    useEffect(() => {
-        if (status === "unauthenticated") {
-            router.push('/auth');
-            router.refresh();
-        } else {
-            if (session !== undefined && session?.user.role !== 'admin') {
-                router.push('/auth');
-                router.refresh();
-            }
-        }
-    }, [router, session, status, session?.user.role]);
+  return selectedEvent != null ? (
+    <div className="min-h-screen flex flex-col px-6 py-6 md:px-24 md:py-8">
+      <h1 className="text-3xl font-medium">Tambah Panitia Baru</h1>
+      <p className="mt-2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, iste.
+      </p>
 
-    return (
-        <>
-        {selectedEvent != null ? (
-            <div className="min-h-screen flex flex-col px-6 py-6 md:px-24 md:py-8">
-                <h1 className="text-3xl font-medium">Tambah Panitia Baru</h1>
-                <p className="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, iste.</p>
+      <form onSubmit={testSubmit} className="flex flex-col gap-5 mt-6">
+        <div className="flex flex-col gap-1 w-full">
+          <label htmlFor="username" className="font-medium">
+            Username
+          </label>
+          <input
+            type="text"
+            name="username"
+            onChange={(e) => setUsername(e.target.value)}
+            className="focus:outline-none focus:ring-2 ring-1 focus:ring-[#7209b7]/80 rounded-md py-2 px-3"
+          />
+        </div>
+        <PasswordToggleInput
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="flex flex-col gap-1 w-full">
+          <label htmlFor="name" className="font-medium">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+            className="focus:outline-none focus:ring-2 ring-1 focus:ring-[#7209b7]/80 rounded-md py-2 px-3"
+          />
+        </div>
+        <div className="flex flex-col gap-1 w-full">
+          <label htmlFor="eventId" className="font-medium">
+            Event
+          </label>
+          <select
+            disabled={true}
+            name="eventId"
+            onChange={(e) => setEventId(e.target.value)}
+            className="focus:outline-none focus:ring-2 ring-1 focus:ring-[#7209b7]/80 rounded-md py-2 px-3"
+          >
+            <option value={selectedEvent.id}>{selectedEvent.name}</option>
+          </select>
+        </div>
 
-                <form onSubmit={testSubmit} className="flex flex-col gap-5 mt-6">
-                    <div className="flex flex-col gap-1 w-full">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" name="username" onChange={(e) => setUsername(e.target.value)} className="focus:outline-none focus:ring-2 ring-1 focus:ring-[#7209b7]/80 rounded-md py-2 px-3"/>
-                    </div>
-                    <PasswordToggleInput value={password} onChange={(e) => setPassword(e.target.value)} />
-                    <div className="flex flex-col gap-1 w-full">
-                        <label htmlFor="name">Name</label>
-                        <input type="text" name="name" onChange={(e) => setName(e.target.value)} className="focus:outline-none focus:ring-2 ring-1 focus:ring-[#7209b7]/80 rounded-md py-2 px-3"/>
-                    </div>
-                    <div className="flex flex-col gap-1 w-full">
-                        <label htmlFor="eventId">Event</label>
-                        <select disabled={true} name="eventId" onChange={(e) => setEventId(e.target.value)} className="focus:outline-none focus:ring-2 ring-1 focus:ring-[#7209b7]/80 rounded-md py-2 px-3">
-                            <option value={selectedEvent.id}>{selectedEvent.name}</option>
-                        </select>
-                    </div>
+        {error && <ErrorMessage message={error} />}
 
-                    {error && <ErrorMessage message={error} />}
-
-                    <button disabled={loading} type="submit" className="p-3 rounded-lg bg-[#7209b7] text-white hover:bg-[#7209b7]/80 active:bg-[#7209b7]/80 font-semibold disabled:grayscale-50 transition duration-200 w-full text-center">{loading ? 'Tunggu...' : 'Submit'}</button>
-                </form>
-            </div>
-        ) : (
-            <div className="min-h-screen flex flex-col justify-center items-center">
-                <h1 className="font-semibold text-2xl">Event Tidak Tersedia</h1>
-            </div>
-        )}
-        </>
-    )
+        <button
+          disabled={loading}
+          type="submit"
+          className="p-3 rounded-lg bg-[#7209b7] text-white hover:bg-[#7209b7]/80 active:bg-[#7209b7]/80 font-semibold disabled:grayscale-50 transition duration-200 w-full text-center"
+        >
+          {loading ? "Tunggu..." : "Submit"}
+        </button>
+      </form>
+    </div>
+  ) : (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <h1 className="font-semibold text-2xl">Event Tidak Tersedia</h1>
+    </div>
+  );
 }
--- src/app/(private)/admin/page.jsx
+++ src/app/(private)/admin/page.jsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -7,51 +7,68 @@ import { useEffect } from "react";
 import { events } from "@/data/events";
 
 export default function Page() {
-    const { data: session, status } = useSession();
+  const { data: session, status } = useSession();
 
-    const router = useRouter();
+  const router = useRouter();
 
-    // const myEvents = events.filter((e) => e.userId === session?.user.id);
-    const myEvents = events.filter((e) => e.userId === 1);
+  const myEvents = events.filter((e) => e.userId === session?.user.id);
 
-    useEffect(() => {
-        if (status === "unauthenticated") {
-            router.push('/auth');
-            router.refresh();
-        } else {
-            if (session !== undefined && session?.user.role !== 'admin') {
-                router.push('/auth');
-                router.refresh();
-            }
-        }
-    }, [router, session, status, session?.user.role]);
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/auth");
+      router.refresh();
+    } else {
+      if (session !== undefined && session?.user.role !== "admin") {
+        router.push("/auth");
+        router.refresh();
+      }
+    }
+  }, [router, session, status, session?.user.role]);
 
-    return (
-        <div className="flex flex-col justify-center gap-5 px-6 py-6 md:px-24 md:py-8">
-            <h1 className="text-3xl font-semibold">Admin Page</h1>
-            <p className="text-lg font-thin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis dolorem quisquam quos incidunt sed optio repellat suscipit non consectetur at.</p>
+  return (
+    <div className="flex flex-col justify-center gap-5 px-6 py-6 md:px-24 md:py-8">
+      <h1 className="text-3xl font-semibold">Admin Page</h1>
+      <p className="text-lg font-thin">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
+        dolorem quisquam quos incidunt sed optio repellat suscipit non
+        consectetur at.
+      </p>
 
-            <div className="flex flex-col gap-2 p-5 rounded-lg outline-1 outline-gray-500/50">
-                <h1 className="text-xl font-medium">Tambah Event Baru</h1>
-                <p className="font-thin">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eius.</p>
-                <Link href="/admin/add-event" className="w-fit bg-[#f72585] mt-4 text-white font-semibold rounded-md py-2 px-3 active:bg-[#f72585]/80 hover:bg-[#f72585]/80 transition duration-200">Tambah Event</Link>
+      <div className="flex flex-col gap-2 p-5 rounded-lg outline-1 outline-gray-500/50">
+        <h1 className="text-xl font-medium">Tambah Event Baru</h1>
+        <p className="font-thin">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eius.
+        </p>
+        <Link
+          href="/admin/add-event"
+          className="w-fit bg-[#f72585] mt-4 text-white font-semibold rounded-md py-2 px-3 active:bg-[#f72585]/80 hover:bg-[#f72585]/80 transition duration-200"
+        >
+          Tambah Event
+        </Link>
+      </div>
+
+      <div className="flex flex-col items-center gap-3 mt-4">
+        <h1 className="text-2xl font-medium md:self-start">Event Saya</h1>
+        {myEvents && myEvents.length > 0 ? (
+          myEvents.map((event) => (
+            <div
+              key={event.id}
+              className="p-3 rounded-lg shadow-sm flex flex-col gap-2 text-black w-full max-w-md"
+            >
+              <Link href={`/admin/detail?eventId=${event.id}`}>
+                <h2 className="text-lg font-semibold active:opacity-80 hover:underline">
+                  {event.name}
+                </h2>
+              </Link>
+              {/* <Image src={event.image} alt={event.name} width={300} height={300} className="rounded-lg object-cover"/> */}
+              <p className="text-sm opacity-80">{event.description}</p>
+              {/* <p className="text-sm opacity-80">{event.date}</p> */}
             </div>
-
-            <div className="flex flex-col items-center gap-3 mt-4">
-                <h1 className="text-2xl font-medium md:self-start">Event Saya</h1>
-                {myEvents && myEvents.length > 0 ? (
-                    myEvents.map((event) => (
-                        <div key={event.id} className="p-3 rounded-lg shadow-sm flex flex-col gap-2 text-black w-full max-w-md">
-                            <Link href={`/admin/detail?eventId=${event.id}`}><h2 className="text-lg font-semibold active:opacity-80 hover:underline">{event.name}</h2></Link>
-                            {/* <Image src={event.image} alt={event.name} width={300} height={300} className="rounded-lg object-cover"/> */}
-                            <p className="text-sm opacity-80">{event.description}</p>
-                            {/* <p className="text-sm opacity-80">{event.date}</p> */}
-                        </div>
-                    ))
-                ) : (
-                    <p className="text-gray-500 text-sm mt-4">Belum ada event.</p>
-                )}
-            </div>
-        </div>
-    );
+          ))
+        ) : (
+          <p className="text-gray-500 text-sm mt-4">Belum ada event.</p>
+        )}
+      </div>
+    </div>
+  );
 }
